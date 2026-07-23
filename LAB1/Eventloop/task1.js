c353b8f9308428e3ebbeb5d62e49355dea5c05d1
Arnavv28 <chauhanarnav284@gmail.com>
2026-07-23 15:12:50 +0530

    commit 23/07

---
+++ LAB1/Eventloop/task1.js
@@ -0,0 +1,17 @@
+const fun1 = () => {
+    console.log("fun1");
+};
+const fun2 = () => {
+    console.log("fun2");
+};
+const fun3 = () => {
+    console.log("fun3");
+}
+function main(){
+    console.log("main");
+    fun1();
+    fun2();
+    fun3();
+    console.log("end main")
+    };
+    main();
